ff1d151857cb1dee074ed05b8f9999e9b75c8564
nikolasdn@hotmail.com <nikolasdn@hotmail.com>
2018-12-11 22:26:58 +0100

    espeak

--- SpeechGenerator/Program.cs
+++ SpeechGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace SpeechGenerator
 {
@@ -6,7 +7,38 @@ namespace SpeechGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting SpeechGenerator");
+
+            Cmd("espeak.exe -a 200 -p 50 -vnl+m1 \"Mmm, ik heb lekker geslapen\"");
+
+            Console.WriteLine("SpeechGenerator stopped");
+        }
+
+        static string Cmd(string cmd)
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = cmd,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+            try
+            {
+                process.Start();
+                string result = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                return result;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            return "";
         }
     }
 }
