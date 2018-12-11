e3f1b1849345175c46660b139b0e51f4945c7174
Nikolas <nikolasdn@hotmail.com>
2018-12-11 08:01:23 +0100

    first commit

---
+++ SpeechGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpeechGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
