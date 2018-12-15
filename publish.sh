4ba26ae838361111e94dc7f02279316d9eb70b5b
nikolasdn@hotmail.com <nikolasdn@hotmail.com>
2018-12-15 22:27:13 +0100

    make a directory

--- publish.sh
+++ publish.sh
@@ -2,5 +2,6 @@
 
 dotnet publish **/*.csproj -r linux-arm
 
+mkdir publish
 zip -r publish/mainpackage.zip SpeechGenerator/bin/Debug/netcoreapp2.1/linux-arm/publish
 az storage blob upload --account-name mechi --container-name packages --file publish/mainpackage.zip --name mainpackage.zip
