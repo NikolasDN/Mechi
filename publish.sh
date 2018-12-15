12dd70f1a03b2355cbf54ed5bc6f3c0108405798
nikolasdn@hotmail.com <nikolasdn@hotmail.com>
2018-12-15 21:20:29 +0100

    put every in the same mainpackage

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@
 
 dotnet publish **/*.csproj -r linux-arm
 
-zip -r publish/speechgenerator.zip SpeechGenerator/bin/Debug/netcoreapp2.1/linux-arm/publish
-az storage blob upload --account-name mechi --container-name packages --file publish/speechgenerator.zip --name speechgenerator.zip
+zip -r publish/mainpackage.zip SpeechGenerator/bin/Debug/netcoreapp2.1/linux-arm/publish
+az storage blob upload --account-name mechi --container-name packages --file publish/mainpackage.zip --name mainpackage.zip
