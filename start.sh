d56c005065ca34766e780327f61525556a6be2b1
nikolasdn@hotmail.com <nikolasdn@hotmail.com>
2018-12-15 21:25:00 +0100

    fix

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://mechi.blob.core.windows.net/packages/speechgenerator.zip
-unzip speechgenerator.zip
-rm speechgenerator.zip
+wget https://mechi.blob.core.windows.net/packages/mainpackage.zip
+unzip mainpackage.zip
+rm mainpackage.zip
 dotnet ./SpeechGenerator/bin/Debug/netcoreapp2.1/linux-arm/publish/SpeechGenerator.dll
