e85070061ecb6fb46fce1565db47a0e4991bfefd
Nikolas <nikolasdn@hotmail.com>
2018-12-18 21:37:42 +0100

    split start and install

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://mechi.blob.core.windows.net/packages/mainpackage.zip
+unzip -o mainpackage.zip
+rm mainpackage.zip
--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-wget https://mechi.blob.core.windows.net/packages/mainpackage.zip
-unzip -o mainpackage.zip
-rm mainpackage.zip
 cd publish/main
 dotnet ./Robot.dll
