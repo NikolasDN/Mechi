f1f3c816ec6e99856d8e7d236d586c6bb05e707d
Nikolas <nikolasdn@hotmail.com>
2018-12-18 21:24:24 +0100

    fix

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 wget https://mechi.blob.core.windows.net/packages/mainpackage.zip
-unzip mainpackage.zip
+unzip -o mainpackage.zip
 rm mainpackage.zip
 dotnet ./publish/main/Robot.dll
