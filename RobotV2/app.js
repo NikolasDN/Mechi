4c4eeca7d2afb9a40ab0206f110a1c69f29ebf6b
Nikolas <nikolasdn@hotmail.com>
2018-12-25 11:29:12 +0100

    teaching

---
+++ RobotV2/app.js
@@ -0,0 +1,14 @@
+console.log("Hallo allemaal");
+
+console.log("mag ik spelen");
+
+var nummertje1;
+nummertje1 = 3;
+var nummertje2;
+nummertje2 = 8;
+var nummertje3;
+nummertje3 = nummertje1 - nummertje2;
+console.log(nummertje3);
+var naam1 = "lucas";
+var naam2 = "jolan";
+console.log(naam1 + naam2);
