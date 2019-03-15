a1139a498bbaabb720ba873738e75ea912b1330a
Nikolas <nikolasdn@hotmail.com>
2019-03-15 08:15:08 +0100

    cleanup, lucas

--- RobotV2/app.js
+++ RobotV2/app.js
@@ -2,9 +2,6 @@ const {
     cv
   } = require('./vision/utils');
 const { runVideoFaceDetection } = require('./vision/commons');
-//const CronJob = require('cron').CronJob;
-//const express = require('express');
-//const app = express();
 
 const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
 
@@ -20,17 +17,4 @@ function detectFaces(img) {
   return classifier.detectMultiScale(img.bgrToGray(), options).objects;
 }
 
-//const cronjob1 = new CronJob('*/5 * * * * *', () => {
-// setInterval(() => {
-//   console.log('looking');
-//   runVideoFaceDetection(webcamPort, detectFaces);
-// }, 5000);
-
-// const port = process.env.PORT || 2345;
-// app.listen(port, function () {
-//   console.log('app listening on port: ', port);
-//   cronjob1.start();
-// });
-
-//cronjob1.start();
 runVideoFaceDetection(webcamPort, detectFaces);
