c3f9a0298ad2fbafc771fb25d09c30344d671ac5
Nikolas <nikolasdn@hotmail.com>
2020-03-13 23:27:52 +0100

    jeuj

--- RobotV3/app.py
+++ RobotV3/app.py
@@ -6,23 +6,29 @@ cap = cv2.VideoCapture(0)
 
 app = Flask(__name__)
 
+def get_frame():
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret==True:
+            #frame = cv2.flip(frame,0)
+            return frame
+
 @app.route('/')
 def index():
     """Video streaming home page."""
     return render_template('index.html')
 def gen():
     """Video streaming generator function."""
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if ret==True:
-            frame = cv2.flip(frame,0)
-
+    while True:
             # write the flipped frame
             # out.write(frame)
         # while True:
-            # frame = camera.get_frame()
-            yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        frame = get_frame()
+        (flag, encodedImage) = cv2.imencode(".jpg", frame)
+        # if not flag:
+		# 	continue
+        yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
