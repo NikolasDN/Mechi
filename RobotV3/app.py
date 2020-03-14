d6978876125c4bc86cbe333dea825addd06b8d73
Nikolas <nikolasdn@hotmail.com>
2020-03-14 21:18:26 +0100

    docker

--- RobotV3/app.py
+++ RobotV3/app.py
@@ -36,4 +36,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port =80, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
--- RobotV3/readme.md
+++ RobotV3/readme.md
@@ -2,3 +2,7 @@
 
     pip install -r requirements.txt
     flask run
+
+## Docker
+
+    sudo docker-compose up -d --build
