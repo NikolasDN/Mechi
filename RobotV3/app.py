c3cae319820ebee06f3dc17dea5fa94a9b11a347
nikolasdn@hotmail.com <nikolasdn@hotmail.com>
2020-10-14 22:18:19 +0200

    changes

--- RobotV3/app.py
+++ RobotV3/app.py
@@ -1,26 +1,29 @@
 from flask import Flask, render_template, Response
-import numpy as np
-import cv2
+# import numpy as np
+# import cv2
+# Raspberry Pi camera module (requires picamera package, developed by Miguel Grinberg)
+from camera_pi import Camera
 from gpiozero import LEDBoard
 
-cap = cv2.VideoCapture(-1)
+# cap = cv2.VideoCapture(-1)
 
 leds = LEDBoard(17, 18, 15, 27)
 #my_message = "Started";
 
 app = Flask(__name__)
 
-def get_frame():
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if ret==True:
-            #frame = cv2.flip(frame,0)
-            return frame
+# def get_frame():
+#     while(cap.isOpened()):
+#         ret, frame = cap.read()
+#         if ret==True:
+#             #frame = cv2.flip(frame,0)
+#             return frame
 
 @app.route('/')
 def index():
     """Video streaming home page."""
     return render_template('index.html')
+
 @app.route("/switchlight/")
 def switchlight():
     #Moving forward code
@@ -28,23 +31,19 @@ def switchlight():
     print('Green!')
     leds[0].on()
     return "Nothing"
-def gen():
+
+def gen(camera):
     """Video streaming generator function."""
     while True:
-            # write the flipped frame
-            # out.write(frame)
-        # while True:
-        frame = get_frame()
-        (flag, encodedImage) = cv2.imencode(".jpg", frame)
-        if not flag:
-            continue
+        frame = camera.get_frame()
         yield (b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),
+    return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
--- RobotV3/readme.md
+++ RobotV3/readme.md
@@ -1,8 +1,1 @@
-# How to run
-
-    pip3 install -r requirements.txt
-    flask run --host=0.0.0.0
-
-## Docker
-
-    sudo docker-compose up -d --build
+# TODO
